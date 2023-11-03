a8e816edad5faddc2b8f73fc3704e6368e22dac9
Julian <julian@entrenese.com>
2023-11-03 08:40:09 -0500

    [Delete] Folder public_html.

--- webhooks/pediculosis_parasitismo.php
+++ webhooks/pediculosis_parasitismo.php
@@ -1,10 +1,10 @@
 <?php
-// Llamamos la librería d ediaogflow
+// Llamamos la librería de diaogflow
 include_once "../somosioticos/somosioticos_dialogflow.php";
 // llamamos las credenciales definidas en dialogflow en (Fulfillment)
 credenciales('pediculosisparasitismobot','Jul&@n18/');
 
-// identidicamos el intent que golpea la puerta
+// identificamos el intent que golpea la puerta
 if (intent_recibido("calculadora")){
   $valor1 = obtener_variables()['numero1'];
   $valor2 = obtener_variables()['numero2'];
@@ -12,6 +12,14 @@ if (intent_recibido("calculadora")){
 
   // Devolvemos la respuesta a dialogflow
   enviar_texto("Luego de sumar los valores te digo que el resultado es: $resultado");
+}
+
+if (intent_recibido("Tomar_Ordenar")){
+  $cantidad1 = obtener_variables()['cantidad1'];
+  $producto1 = obtener_variables()['producto1'];
+
+  // Devolvemos la respuesta a dialogflow
+  enviar_texto("La orden es: $cantidad1 $producto1");
 }
 
 
